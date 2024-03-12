9baeb60e865126a6db7e51cd21aa38a4660bf16e
Mạnh Đạt Võ <dat.vomanh@haravan.com>
2024-03-12 15:42:06 +0700

    feat: add test library

---
+++ src/__test__/App.test.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App.jsx';
+
+describe('App tests', () => {
+    it('should contains the heading 1', () => {
+        render(<App />);
+        const heading = screen.getByText(/Hello world! I am using React/i);
+        expect(heading).toBeInTheDocument()
+    });
+});
